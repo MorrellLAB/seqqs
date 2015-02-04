322ab4fc2c5140cadc0e5ee44c2bb509dd5b9250
Tom Kono <konox006@umn.edu>
2015-02-04 17:21:03 -0600

    Add encoding information to trim_autoplot.sh

--- wrappers/trim_autoplot.sh
+++ wrappers/trim_autoplot.sh
@@ -9,6 +9,7 @@ set -u
 set -o pipefail
 
 VERSION=0.4
+SCRIPTNAME=`basename $0`
 
 # path to applications
 #SOFTWARE=~/shared/Software
@@ -20,7 +21,7 @@ SEQQS=~/Dropbox/GitHub/seqqs/seqqs
 
 usage() {
     echo -e "\
-usage: trim.sh sample_name in1.fq in2.fq outdir [adapters prior qual_thresh stats]\n\
+usage: $SCRIPTNAME sample_name in1.fq in2.fq outdir [adapters prior qual_thresh stats]\n\
 	sample_name	sample name for both paired-end files \n\
 	in1.fq.gz     	paired-end file 1\n\
 	in2.fq.gz      	paired-end file 2\n\
@@ -66,7 +67,7 @@ OUTDIR=$4
 ADAPTERS=~/tmp/scythe/illumina_adapters.fa
 
 # scythe's prior
-PRIOR=${6:-0.4}
+PRIOR=${6:-0.04}
 
 # sickle's quality threshold
 QUAL_THRESH=${7:-20}
@@ -74,16 +75,21 @@ QUAL_THRESH=${7:-20}
 # stat directory
 STAT=${8:-$OUTDIR/stats}
 
+#   Which FASTQ encoding are we using?
+#   This is one of sanger, illumina, or solexa
+#   we default to illumina
+ENCODING=solexa
+
 check_file_exists() {
     if [ ! -e "$1" ]; then 
-	echo "[trim.sh] error: file '$1' does not exist." >&2
+	echo "[$SCRIPTNAME] error: file '$1' does not exist." >&2
 	exit 1
     fi
 }
 
 check_dir_exists() {
     if [ ! -d "$1" ]; then 
-	echo "[trim.sh] message: directory '$1' does not exist, creating it." >&2
+	echo "[$SCRIPTNAME] message: directory '$1' does not exist, creating it." >&2
 	mkdir -p $1
     fi
 }
@@ -91,7 +97,7 @@ check_dir_exists() {
 # removing this checking for path, but should perhaps replace with checking for program
 ## Check that programs are in $PATH
 ((command -v $SCYTHE && command -v $SICKLE && command -v $SEQQS) > /dev/null) || \
-    (echo "[trim.sh] error: either scythe, sickcle, or seqqs is not in your $PATH" && exit 1))
+    (echo "[$SCRIPTNAME] error: either scythe, sickcle, or seqqs is not in your $PATH" && exit 1)
 
 ## Check input
 check_file_exists $ADAPTERS
@@ -101,7 +107,7 @@ check_dir_exists $STAT
 check_dir_exists $OUTDIR
 
 if [ ! ${#SAMPLE_NAME} -gt 0 ]; then
-    echo "[trim.sh] error: specify sample name" >&2
+    echo "[$SCRIPTNAME] error: specify sample name" >&2
     exit
 fi
 
@@ -110,12 +116,12 @@ echo "[trim.sh] running sample '$SAMPLE_NAME' with input PE files '$IN1'/'$IN2'.
 # time trimming process process
 T="$(date +%s)"
 
-$SICKLE pe -t sanger -q $QUAL_THRESH \
-    -f <($SEQQS -e -p $STAT/raw_${SAMPLE_NAME}_R1 "$IN1" | $SCYTHE -a "$ADAPTERS" -p $PRIOR - 2> $STAT/${SAMPLE_NAME}_R1_scythe.stderr) \
-    -r <($SEQQS -e -p $STAT/raw_${SAMPLE_NAME}_R2 "$IN2" | $SCYTHE -a "$ADAPTERS" -p $PRIOR - 2> $STAT/${SAMPLE_NAME}_R2_scythe.stderr) \
-    -o >($SEQQS -e -p $STAT/trimmed_${SAMPLE_NAME}_R1 - | gzip > $OUTDIR/${SAMPLE_NAME}_R1_trimmed.fq.gz) \
-    -p >($SEQQS -e -p $STAT/trimmed_${SAMPLE_NAME}_R2 - | gzip > $OUTDIR/${SAMPLE_NAME}_R2_trimmed.fq.gz) \
-    -s >($SEQQS -e -p $STAT/trimmed_${SAMPLE_NAME}_singles - | gzip > $OUTDIR/${SAMPLE_NAME}_singles_trimmed.fq.gz) > $STAT/${SAMPLE_NAME}_sickle.stderr
+$SICKLE pe -t ${ENCODING} -q $QUAL_THRESH \
+    -f <($SEQQS -e -q ${ENCODING} -p $STAT/raw_${SAMPLE_NAME}_R1 "$IN1" | $SCYTHE -a "$ADAPTERS" -p $PRIOR - 2> $STAT/${SAMPLE_NAME}_R1_scythe.stderr) \
+    -r <($SEQQS -e -q ${ENCODING} -p $STAT/raw_${SAMPLE_NAME}_R2 "$IN2" | $SCYTHE -a "$ADAPTERS" -p $PRIOR - 2> $STAT/${SAMPLE_NAME}_R2_scythe.stderr) \
+    -o >($SEQQS -e -q ${ENCODING} -p $STAT/trimmed_${SAMPLE_NAME}_R1 - | gzip > $OUTDIR/${SAMPLE_NAME}_R1_trimmed.fq.gz) \
+    -p >($SEQQS -e -q ${ENCODING} -p $STAT/trimmed_${SAMPLE_NAME}_R2 - | gzip > $OUTDIR/${SAMPLE_NAME}_R2_trimmed.fq.gz) \
+    -s >($SEQQS -e -q ${ENCODING} -p $STAT/trimmed_${SAMPLE_NAME}_singles - | gzip > $OUTDIR/${SAMPLE_NAME}_singles_trimmed.fq.gz) > $STAT/${SAMPLE_NAME}_sickle.stderr
 
 T="$(($(date +%s)-T))"
 echo "[trim.sh] $SAMPLE_NAME took seconds: ${T}"
